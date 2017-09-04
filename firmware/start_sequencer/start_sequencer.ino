54d69a41d28855851f005b113584b5ddd319f5a6
Andrew G. Meyer <agmlego@gmail.com>
2017-09-04 14:21:00 -0400

    Leftover changes from decommissioned machine

--- firmware/start_sequencer/start_sequencer.ino
+++ firmware/start_sequencer/start_sequencer.ino
@@ -12,66 +12,66 @@ time_t cur_time;
 time_t start_time;
 
 void setup(){
-	// pulse-per-second interrupt from DS3231
-	pinMode(PPS_INT_PIN, INPUT);
-	attachInterrupt(PPS_INT, pps_isr, FALLING);
-	
-	// remote-trigger interrupt from pendant
-	pinMode(TRIGGER_INT_PIN, INPUT);
-	attachInterrupt(TRIGGER_INT, trigger_isr, FALLING);
-	
-	// LCD backlight and contrast PWM
-	pinMode(LCD_BACKLIGHT, OUTPUT);
-	analogWrite(LCD_BACKLIGHT, 127);
-	pinMode(LCD_CONTRAST, OUTPUT);
-	analogWrite(LCD_CONTRAST, 127);
-	
-	// button backlight PWM
-	pinMode(BUTTON_BACKLIGHT, OUTPUT);
-	analogWrite(BUTTON_BACKLIGHT, 127);
-	
-	// button matrix input
-	pinMode(BUTTON_MATRIX, INPUT);
-	
-	// horn and camera outputs
-	pinMode(HORN, OUTPUT);
-	pinMode(CAMERA, OUTPUT);
-	
-	// ambient light sensor
-	pinMode(CDS_CELL, INPUT);
-	
-	// splash screen
-	lcd_buffer0 = "Creekfleet Start";
-	lcd_buffer1 = " Sequencer v1.0 ";
-	lcd.print(lcd_buffer0);
-	lcd.print(lcd_buffer1);
-	
-	// initialise time values to a sentinel value
-	cur_time = -1;
-	start_time = -1;
-	
-	// debug serial output
-	Serial.begin(115200);
-	Serial.println("I live!");
+    // pulse-per-second interrupt from DS3231
+    pinMode(PPS_INT_PIN, INPUT);
+    attachInterrupt(PPS_INT, pps_isr, FALLING);
+    
+    // remote-trigger interrupt from pendant
+    pinMode(TRIGGER_INT_PIN, INPUT);
+    attachInterrupt(TRIGGER_INT, trigger_isr, FALLING);
+    
+    // LCD backlight and contrast PWM
+    pinMode(LCD_BACKLIGHT, OUTPUT);
+    analogWrite(LCD_BACKLIGHT, 127);
+    pinMode(LCD_CONTRAST, OUTPUT);
+    analogWrite(LCD_CONTRAST, 127);
+    
+    // button backlight PWM
+    pinMode(BUTTON_BACKLIGHT, OUTPUT);
+    analogWrite(BUTTON_BACKLIGHT, 127);
+    
+    // button matrix input
+    pinMode(BUTTON_MATRIX, INPUT);
+    
+    // horn and camera outputs
+    pinMode(HORN, OUTPUT);
+    pinMode(CAMERA, OUTPUT);
+    
+    // ambient light sensor
+    pinMode(CDS_CELL, INPUT);
+    
+    // splash screen
+    lcd_buffer0 = "Creekfleet Start";
+    lcd_buffer1 = " Sequencer v1.0 ";
+    lcd.print(lcd_buffer0);
+    lcd.print(lcd_buffer1);
+    
+    // initialise time values to a sentinel value
+    cur_time = -1;
+    start_time = -1;
+    
+    // debug serial output
+    Serial.begin(115200);
+    Serial.println("I live!");
 }
 
 void loop()
 {
-	cur_time = now();
-	
+    cur_time = now();
+    
 }
 
 void pps_isr()
 {
-	
+    
 }
 
 void trigger_isr()
 {
-	if start_time != -1)
-	{
-		time_t diff = cur_time - start_time;
-		lcd_buffer0 = sprintf();
-		lcd.print(lcd_buffer0);
-	}
+    if(start_time != -1)
+    {
+        time_t diff = cur_time - start_time;
+        lcd_buffer0 = sprintf();
+        lcd.print(lcd_buffer0);
+    }
 }
